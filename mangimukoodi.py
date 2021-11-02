b71bcfb34b5babf08cc04f073a3977ec9efe2c0a
Johann Mattias Kammiste <johannmkammiste@gmail.com>
2021-11-02 19:23:26 +0200

    Changes to code

--- mangimukoodi.py
+++ mangimukoodi.py
@@ -4,21 +4,33 @@ from music21 import instrument, note, chord, stream
 
 def mangimukoodi(failinimi):
     fail = open(failinimi, "r+", encoding="UTF-8")
-    failisisu = []
-    stream1 = stream.Stream() #stream on nootide list pmst.
-    reanr = 60 #alustan noodist C
-    for rida in fail:
-        reanr -= 1
-        if reanr >= 0:
-            failisisu = rida.split()
-            for i in range(len(failisisu)):
-                noot = note.Note(reanr)
-                noot.duration.quarterLength = len(failisisu[i])
-                stream1.append(noot)
+    ridadesisu = fail.readlines()
+    koodipikkus = len(ridadesisu)
+    nootidestream = stream.Stream() #stream on nootide list pmst.
+    veerud = [] #meil oleks vaja see fail muuta veergudeks ja need veerud peaksid olema ka tühjadena. Vbl peaks leidma kõige pikema rea ja selle järgi tegema.
+    max_pikkus = 0
+    alampiir = 21 #MIDIS on 0-127 nooti, aga klaveri nootidest on 21 kuni 108
+    # if koodipikkus < (128 + alampiir):                #Ma selle kommentin hetkel välja, homme tegelen sellega.
+    #     for mitmesrida in range(koodipikkus):
+    #         for rida in ridadesisu:
+    #             ajutine_list = []
+    #             for el in rida.split():
+    #                 ajutine_list.append(el)
+    #             veerud.append(list(ajutine_list))
+
+        if koodipikkus > alampiir:
+            koodipikkus -= 1
+            akordid = chord.Chord()
+            for veerusisu in veerud:
+                for count, value in enumerate(veerusisu):
+                    noot = note.Note(koodipikkus,quarterLength=len(reasisu[count]))
+                    akordid.add(noot)
+                nootidestream.append(akordid)
     pygame.init()
-    stream1.write('midi', fp=failinimi.split("/")[-1].replace(".py", ".mid")) #see salvestab faili 
-    pygame.mixer.music.load(fail)
-    pygame.mixer.music.play() #see miskiprst ei toimi hetkel
+    uus_fail = failinimi.split("/")[-1].replace(".py", ".mid")
+    nootidestream.write('midi', fp=uus_fail) #see salvestab faili
+    pygame.mixer.music.load(uus_fail)
+    pygame.mixer.music.play()
 
             
 failinimi = input("Sisesta faili nimi, mida tahad kuulda: ")    
