83760297fb6b3586c9ecfcb2918e998371683407
Johann Mattias Kammiste <johannmkammiste@gmail.com>
2021-11-02 16:00:44 +0200

    Update mangimukoodi.py

--- mangimukoodi.py
+++ mangimukoodi.py
@@ -1,6 +1,25 @@
-import pygame
 import sys
+import pygame
+from music21 import instrument, note, chord, stream
 
-pygame.init()
-pygame.mixer.music.load()  # failinimi
-pygame.mixer.music.play()
+def mangimukoodi(failinimi):
+    fail = open(failinimi, "r+", encoding="UTF-8")
+    failisisu = []
+    stream1 = stream.Stream() #stream on nootide list pmst.
+    reanr = 60 #alustan noodist C
+    for rida in fail:
+        reanr -= 1
+        if reanr >= 0:
+            failisisu = rida.split()
+            for i in range(len(failisisu)):
+                noot = note.Note(reanr)
+                noot.duration.quarterLength = len(failisisu[i])
+                stream1.append(noot)
+    pygame.init()
+    stream1.write('midi', fp=failinimi.split("/")[-1].replace(".py", ".mid")) #see salvestab faili 
+    pygame.mixer.music.load(fail)
+    pygame.mixer.music.play() #see miskiprst ei toimi hetkel
+
+            
+failinimi = input("Sisesta faili nimi, mida tahad kuulda: ")    
+mangimukoodi(failinimi)
