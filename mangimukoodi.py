992d725e78569a20b5d74980d23a25751d82a4a7
Johann Mattias Kammiste <johannmkammiste@gmail.com>
2021-11-03 20:27:34 +0200

    Additions to program

--- mangimukoodi.py
+++ mangimukoodi.py
@@ -1,37 +1,40 @@
 import sys
 import pygame
+import numpy as np
 from music21 import instrument, note, chord, stream
+
+def pikimrida(failinimi):
+    pikkus = 0
+    failisisu = open(failinimi, "r+", encoding="UTF-8")
+    for rida in failisisu:
+        if len(rida) > pikkus:
+            pikkus = len(rida)
+    return pikkus
 
 def mangimukoodi(failinimi):
     fail = open(failinimi, "r+", encoding="UTF-8")
-    ridadesisu = fail.readlines()
-    koodipikkus = len(ridadesisu)
+    failisisu= fail.readlines()
+    pikimpikkus = pikimrida(failinimi)
+    failipikkus = len(failisisu)
+    noodistikupikkus = 0
+    if pikimpikkus > failipikkus:
+        noodistikupikkus = pikimpikkus
+    elif pikimpikkus <= failipikkus:
+        noodistikupikkus = failipikkus
     nootidestream = stream.Stream() #stream on nootide list pmst.
-    veerud = [] #meil oleks vaja see fail muuta veergudeks ja need veerud peaksid olema ka tühjadena. Vbl peaks leidma kõige pikema rea ja selle järgi tegema.
-    max_pikkus = 0
+    akordid = chord.Chord()
+    akordidemaatriks = np.array([], ndmin=noodistikupikkus)
     alampiir = 21 #MIDIS on 0-127 nooti, aga klaveri nootidest on 21 kuni 108
-    # if koodipikkus < (128 + alampiir):                #Ma selle kommentin hetkel välja, homme tegelen sellega.
-    #     for mitmesrida in range(koodipikkus):
-    #         for rida in ridadesisu:
-    #             ajutine_list = []
-    #             for el in rida.split():
-    #                 ajutine_list.append(el)
-    #             veerud.append(list(ajutine_list))
+    for rida in failisisu:
+        realemendid = []
+        reaelemendid = rida.split()
+        if len(reaelemendid) < pikimpikkus:
+            pikkusevahe = pikimpikkus - len(reaelemendid)
+            for i in range(pikkusevahe):
+                reaelemendid.append("^0^")
+            np.r_[akordidemaatriks,[reaelemendid]]
 
-        if koodipikkus > alampiir:
-            koodipikkus -= 1
-            akordid = chord.Chord()
-            for veerusisu in veerud:
-                for count, value in enumerate(veerusisu):
-                    noot = note.Note(koodipikkus,quarterLength=len(reasisu[count]))
-                    akordid.add(noot)
-                nootidestream.append(akordid)
-    pygame.init()
-    uus_fail = failinimi.split("/")[-1].replace(".py", ".mid")
-    nootidestream.write('midi', fp=uus_fail) #see salvestab faili
-    pygame.mixer.music.load(uus_fail)
-    pygame.mixer.music.play()
-
-            
+    print(akordidemaatriks)
+ 
 failinimi = input("Sisesta faili nimi, mida tahad kuulda: ")    
 mangimukoodi(failinimi)
