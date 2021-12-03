158ef27dd985ca2de5d5796d55fa038efd874252
Anne-Mari Kasemetsa <annemari.kasemetsa3@gmail.com>
2021-12-04 00:36:39 +0200

    kindlate nootidega variant

--- auh.py
+++ auh.py
@@ -1,28 +1,24 @@
 import sys
-# import pygame
+import random
 from music21 import instrument, note, chord, stream
+
+elemendid = "AaBbCcDdeEfFgGhHiIjJkKlLmMnNoOpPqQrRsSšŠzZžŽtTuUvVWwõÕäÄöÖüÜx\"XyY<>*,._#¤%&/?!-;:^'1234567890´=`\{}()[]€+$£@ˇ~ ½"
+lst = list(elemendid)
+pikkus = len(lst)
 
 def mangimukoodi(failinimi):
     fail = open(failinimi, "r+", encoding="UTF-8")
-    failisisu = []
-    stream1 = stream.Stream() #stream on nootide list pmst.
-    reanr = 60 #alustan noodist A
+    stream1 = stream.Stream() 
     for rida in fail:
-        reanr -= 1
-        if reanr >= 0:
-            failisisu = rida.split()
-            for i in range(len(failisisu)):
-                noot = note.Note(reanr)
-                noot.duration.quarterLength = len(failisisu[i]) / 8
+        puhas = rida.strip()
+        failisisu = puhas.split(" ")
+        for el in failisisu:
+            for i in el:
+                kus = random.randrange(0, pikkus)
+                noot = note.Note(kus)
+                noot.duration.quarterLength = 1 / 8
                 stream1.append(noot)
-    # pygame.init()
     stream1.write('midi', fp=failinimi.split("/")[-1].replace(".py", ".mid")) #see salvestab faili 
-    # pygame.mixer.music.load(fail)
-    # pygame.mixer.music.play() #see miskiprst ei toimi hetkel
-
-# pygame.init()
-# pygame.mixer.music.load()  # failinimi
-# pygame.mixer.music.play()
             
 failinimi = input("Sisesta faili nimi, mida tahad kuulda: ")    
 mangimukoodi(failinimi)
